5488fb05d09070de8bb73c6301287d5527e41762
happyGyu <happyboyto.dev@gmail.com>
2022-08-02 13:46:28 +0900

    feat: cra로 리액트 프로젝트 시작
    - 타입스크립트 적용
    - eslint, prettier 적용
    - styled-components 적용 및 글로벌 스타일 적용
    
    - #1

---
+++ client/src/style/GlobalStyle.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import colors from '../constants/colors';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *{
+    box-sizing: border-box;
+  }
+  button{
+    margin:0;
+    padding:0;
+    border:none;
+    background-color:inherit;
+    border-radius: inherit;
+    cursor:pointer;
+  }
+  input{
+    border:none;
+    border-radius:inherit;
+    margin:0;
+    padding:0;
+    :focus-visible{
+      outline:none;
+    }
+  }
+  textarea{
+    margin:0;
+    padding:0;
+    border:none;
+    height:auto;
+    max-width:100%;
+    background-color:inherit;
+    border-radius:inherit;
+    color:inherit;
+    :focus-visible{
+      outline:none;
+    }
+  }
+  a{
+    color:inherit;
+    text-decoration:none;
+  }
+  body{
+    background-color: ${colors.background};
+    color: ${colors.titleActive};
+  }
+`;
+
+export default GlobalStyle;
