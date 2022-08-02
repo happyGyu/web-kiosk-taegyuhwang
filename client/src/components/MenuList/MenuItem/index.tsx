d5e9823191a0cce380ccb0b542648f3b1bea6bc1
happyGyu <happyboyto.dev@gmail.com>
2022-08-03 01:38:18 +0900

    feat: 메뉴 리스트 마크업
    - 기본 마크업 진행
    
    - #2

---
+++ client/src/components/MenuList/MenuItem/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../../../constants/colors';
+import { IMenu } from '../../../types';
+import mixin from '../../../style/mixin';
+
+export default function MenuItem({
+  id,
+  name,
+  basePrice,
+  imgUrl,
+  isSoldOut,
+}: IMenu) {
+  return (
+    <MenuItemContainer isSoldOut={isSoldOut}>
+      <MenuImage imgUrl={imgUrl} />
+      <MenuTitle>{name}</MenuTitle>
+    </MenuItemContainer>
+  );
+}
+
+const MenuItemContainer = styled.li<{ isSoldOut: IMenu['isSoldOut'] }>`
+  ${mixin.flexMixin({
+    direction: 'column',
+    justify: 'space-between',
+    align: 'center',
+  })}
+  gap: 1rem;
+  width: calc((100% - 2rem) / 3);
+  height: 12rem;
+  padding: 1rem 0 3rem 0;
+  background: ${colors.offWhite};
+  border: ${({ isSoldOut }) => (isSoldOut ? '2px solid red' : '')};
+`;
+
+const MenuImage = styled.img<{ imgUrl: IMenu['imgUrl'] }>`
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+  border-radius: 50%;
+  content: url(${({ imgUrl }) => imgUrl});
+`;
+
+const MenuTitle = styled.span`
+  font-size: 1.25rem;
+  font-weight: 700;
+`;
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,3 +2,11 @@ export interface IMenuCategory {
   categoryId: number;
   categoryName: string;
 }
+
+export interface IMenu {
+  id: number;
+  name: string;
+  basePrice: number;
+  imgUrl: string;
+  isSoldOut: boolean;
+}
