a4ec44b8dc4a855f1d48131be3a03480abcf7200
happyGyu <happyboyto.dev@gmail.com>
2022-08-04 02:28:44 +0900

    feat: db 연결에 필요한 모듈 설치
    - 환경변수 적용을 위해 dotenv 설치
    - mysql2 설치
    
    - #4

--- server/src/typeorm.config.ts
+++ server/src/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOSTNAME,
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER_NAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/**/entities/*.entity.{js,ts}'],
+  entities: [__dirname + '/**/*.entity.{js,ts}'],
   synchronize: process.env.NODE_ENV === 'development',
 };
