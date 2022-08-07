3cd3231096b983b5178108e8cdd844ace67f1b1e
happyGyu <happyboyto.dev@gmail.com>
2022-08-08 00:59:18 +0900

    feat: 카테고리별 매출 순위를 조회하는 API 구현
    - 매출 순위를 배열 형태로 응답합니다
    - 매출이 없으면 배열에 들어있지 않습니다
    
    - #12

--- server/src/menu/menu.controller.ts
+++ server/src/menu/menu.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Query, Res } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { Response } from 'express';
 
@@ -16,5 +16,16 @@ export class MenuController {
   async getAllCategories(@Res() res: Response) {
     const categories = await this.menuService.getAllCategories();
     return res.status(HttpStatus.OK).json({ categories });
+  }
+
+  @Get('ranking')
+  async getSalesRanking(
+    @Res() res: Response,
+    @Query('category-id') categoryId: string,
+  ) {
+    const salesRanking = await this.menuService.getSalesRanking(
+      parseInt(categoryId),
+    );
+    return res.status(HttpStatus.OK).json({ salesRanking });
   }
 }
--- server/src/menu/menu.service.ts
+++ server/src/menu/menu.service.ts
@@ -1,3 +1,4 @@
+import { SoldMenu } from 'src/order/entities/soldMenu.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -26,5 +27,41 @@ export class MenuService {
 
   async getById(id: number): Promise<Menu> {
     return await this.menuRepository.findOneBy({ id });
+  }
+
+  async getMenusWithSalesLog(categoryId: number) {
+    return await this.menuRepository.find({
+      relations: ['soldMenus'],
+      select: ['id', 'name', 'soldMenus'],
+      where: { category: { id: categoryId } },
+    });
+  }
+
+  calculateTotalSoldQuantities(soldMenus: SoldMenu[]) {
+    return soldMenus.reduce(
+      (totalQuantity, soldMenu) => totalQuantity + soldMenu.quantity,
+      0,
+    );
+  }
+
+  getMenusWithTotalSalesLog(menusWithSalesLog: Menu[]) {
+    return menusWithSalesLog.map((menuWithSalesLog) => {
+      const { id, name, soldMenus } = menuWithSalesLog;
+      const totalSoldQuantity = this.calculateTotalSoldQuantities(soldMenus);
+      return {
+        id,
+        name,
+        totalSoldQuantity,
+      };
+    });
+  }
+
+  async getSalesRanking(categoryId: number) {
+    const menusWithSalesLog = await this.getMenusWithSalesLog(categoryId);
+    const menusWithTotalSalesLog =
+      this.getMenusWithTotalSalesLog(menusWithSalesLog);
+    return menusWithTotalSalesLog.sort(
+      (a, b) => b.totalSoldQuantity - a.totalSoldQuantity,
+    );
   }
 }
