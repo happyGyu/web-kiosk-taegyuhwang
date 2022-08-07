283c5765844e0fc659660762fdb5ab530027f4b7
happyGyu <happyboyto.dev@gmail.com>
2022-08-07 12:18:28 +0900

    feat: 일반적으로 사용되는 container 컴포넌트 구현
    - 컴포넌트를 감싸는 역할을 하는 컨테이너는 여러 곳에서 필요하기 때문에 추상화하여 구현
    
    - #4

---
+++ client/src/components/common/Container.tsx
@@ -0,0 +1,73 @@
+import mixin from 'style/mixin';
+import styled, { css } from 'styled-components';
+
+type FlexTypes = {
+  direction?: 'row' | 'column';
+  align?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around';
+  justify?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around';
+  wrap?: 'wrap' | 'no-wrap';
+};
+
+const Container = styled.div<{
+  width?: string;
+  height?: string;
+  margin?: string;
+  mt?: string;
+  mb?: string;
+  ml?: string;
+  mr?: string;
+  padding?: string;
+  flexInfo?: FlexTypes;
+  gap?: number;
+  position?: string;
+  left?: string;
+  right?: string;
+  top?: string;
+  bottom?: string;
+}>`
+  ${({
+    width,
+    height,
+    margin,
+    mt,
+    mb,
+    ml,
+    mr,
+    padding,
+    gap,
+    position,
+    left,
+    right,
+    top,
+    bottom,
+  }) => css`
+    ${width && `width: ${width};`}
+    ${height && `height: ${height};`}
+    ${margin && `margin: ${margin};`}
+    ${mt && `margin-top: ${mt};`}
+    ${mb && `margin-bottom: ${mb};`}
+    ${ml && `margin-left: ${ml};`}
+    ${mr && `margin-right: ${mr};`}
+    ${padding && `padding: ${padding};`}
+    ${gap && `gap : ${gap}rem;`}
+    ${position && `position : ${position};`}
+    ${left && `left : ${left};`}
+    ${right && `right : ${right};`}
+    ${top && `top : ${top};`}
+    ${bottom && `bottom : ${bottom};`}
+  `}
+
+  ${({ flexInfo }) => flexInfo && mixin.flexMixin(flexInfo)}
+`;
+
+export default Container;
