d0a70c0152ef4fd925442c3c3477ecbbe17d8e8f
happyGyu <happyboyto.dev@gmail.com>
2022-08-07 17:04:59 +0900

    feat: 단일 메뉴에 대한 옵션을 가져오는 API 구현
    - #9

--- server/src/choice/choice.service.ts
+++ server/src/choice/choice.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
-import { Choice } from './entities/choice.entity';
 import { ChoiceGroup } from './entities/choiceGroup.entity';
 
 @Injectable()
 export class ChoiceService {
   constructor(
-    @InjectRepository(Choice)
-    private choiceRepository: Repository<Choice>,
     @InjectRepository(ChoiceGroup)
     private choiceGroupRepository: Repository<ChoiceGroup>,
   ) {}
