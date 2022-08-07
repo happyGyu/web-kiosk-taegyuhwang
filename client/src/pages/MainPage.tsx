ab9cda06eb2fa38540faee5af76be18f7f9920c8
happyGyu <happyboyto.dev@gmail.com>
2022-08-07 10:07:22 +0900

    style: mainpage layout 정리
    - #4

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -2,32 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 import MenuCategoryNav from 'components/MenuCategoryNav';
 import MenuList from 'components/MenuList';
+import Cart from 'components/Cart';
+import mixin from 'style/mixin';
 
 export default function MainPage() {
   return (
-    <>
+    <PageWrapper>
       <Banner />
       <MenuCategoryNav />
       <Main>
         <MenuList />
+        <Cart />
       </Main>
-    </>
+    </PageWrapper>
   );
 }
 
+const PageWrapper = styled.div`
+  ${mixin.flexMixin({
+    direction: 'column',
+    justify: 'space-between',
+  })}
+  height: 100%;
+`;
+
 const Banner = styled.img`
-  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  height: 30%;
+  height: 25%;
   content: url('https://images.unsplash.com/photo-1551418988-c21e451467b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3474&q=80');
 `;
 
 const Main = styled.div`
-  margin-top: 1rem;
-  display: flex;
-  gap: 1rem;
   width: 100%;
-  height: 61%;
+  display: flex;
+  flex-grow: 1;
+  gap: 1rem;
+  background-color: red;
 `;
