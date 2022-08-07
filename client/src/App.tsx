7afa91de5174d72bdd2c37b2339273e1889e90da
happyGyu <happyboyto.dev@gmail.com>
2022-08-07 10:22:55 +0900

    feat: App 스타일 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import GlobalStyle from 'style/GlobalStyle';
 import colors from 'constants/colors';
 import MainPage from 'pages/MainPage';
-import mixin from 'style/mixin';
 
 export default function App() {
   return (
@@ -19,19 +18,17 @@ export default function App() {
 }
 
 const KioskMachine = styled.div`
-  background: black;
+  background: ${colors.titleActive};
   padding: 4rem;
   width: 960px;
   height: 1440px;
   margin: 5rem auto;
-  border-radius: 3rem;
+  border-radius: 2rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
 `;
 
 const Display = styled.div`
-  ${mixin.flexMixin({ direction: 'column', align: 'space-around' })}
   position: relative;
   height: 100%;
   background: ${colors.background};
-  padding: 1rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
 `;
