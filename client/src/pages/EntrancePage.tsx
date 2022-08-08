1b29622d520a46cee61e41a47129ab6ec097c0f0
happyGyu <happyboyto.dev@gmail.com>
2022-08-08 20:33:28 +0900

    feat: 첫 페이지에서 전체 데이터 불러오는 동안 로딩 인디케이터 표시
    - 키오스크에서 필요한 모든 데이터를 첫 화면에서 한번만 불러옵니다
    - 그때 로딩 중임을 감지하고 로딩 인디케이터를 띄웁니다.
    - 로딩 인디케이터를 비롯한 초기화면 디자인은 입히지 않은 상태입니다.
    
    - #14

--- client/src/pages/EntrancePage.tsx
+++ client/src/pages/EntrancePage.tsx
@@ -2,9 +2,31 @@ import styled from 'styled-components';
 import { colors } from 'style/constants';
 import Container from 'components/common/Container';
 import { usePageDispatchContext } from 'store/page/pageContext';
+import useAxios from 'hooks/useAxios';
+import { useEffect, useState } from 'react';
+import {
+  GetMenuCategoriesApiResponseDto,
+  GetMenusApiResponseDto,
+  GetPaymentMethodsApiResponseDto,
+} from 'types';
 
 export default function EntrancePage() {
   const dispatchPage = usePageDispatchContext();
+  const [isInitializing, setIsInitializing] = useState(false);
+
+  const categoriesState =
+    useAxios<GetMenuCategoriesApiResponseDto>('/menus/categories');
+  const menusState = useAxios<GetMenusApiResponseDto>('/menus');
+  const paymentMethodsState =
+    useAxios<GetPaymentMethodsApiResponseDto>('/payment-methods');
+
+  useEffect(() => {
+    const isAnyLoading =
+      categoriesState.isLoading ||
+      menusState.isLoading ||
+      paymentMethodsState.isLoading;
+    setIsInitializing(isAnyLoading);
+  }, [categoriesState, menusState, paymentMethodsState]);
 
   const changePageToMain = () => {
     dispatchPage('MAIN');
@@ -16,7 +38,11 @@ export default function EntrancePage() {
       height="100%"
       flexInfo={{ justify: 'center', align: 'center' }}
     >
-      <StartButton onClick={changePageToMain}>주문하기</StartButton>
+      {isInitializing ? (
+        <LoadingIndicator>로딩중</LoadingIndicator>
+      ) : (
+        <StartButton onClick={changePageToMain}>주문하기</StartButton>
+      )}
     </Container>
   );
 }
@@ -26,4 +52,8 @@ const StartButton = styled.button`
   height: 5rem;
   background-color: ${colors.primary};
   color: ${colors.offWhite};
+`;
+
+const LoadingIndicator = styled.h1`
+  font-size: 5rem;
 `;
