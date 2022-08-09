922bf9fae16ab2e5014ea1bbc27b6a107c6749e0
happyGyu <happyboyto.dev@gmail.com>
2022-08-09 17:43:38 +0900

    feat: cart reducer 구현
    - 기존의 증가, 감소를 수량 변경으로 수정

---
+++ client/src/store/cart/cartReducer.ts
@@ -0,0 +1,38 @@
+import { CartActionType, CartStateType } from './cartContext';
+
+function findTargetItemIdx(targetMenuId: number, cartState: CartStateType) {
+  const targetMenu = cartState.find(
+    (cartItem) => cartItem.menuId === targetMenuId
+  );
+  if (!targetMenu) throw new Error('Something wrong: Invalid menu id');
+  return cartState.indexOf(targetMenu);
+}
+
+export default function cartReducer(
+  state: CartStateType,
+  action: CartActionType
+): CartStateType {
+  switch (action.type) {
+    case 'DELETE': {
+      const targetItemIdx = findTargetItemIdx(action.menuId, state);
+      const newState = [...state].splice(targetItemIdx, 1);
+      return newState;
+    }
+    case 'CHANGE_QUANTITY': {
+      const targetItemIdx = findTargetItemIdx(action.menuId, state);
+      const newState = [...state];
+      newState[targetItemIdx].quantity = action.quantity;
+      return newState;
+    }
+    case 'UPDATE': {
+      const targetItemIdx = findTargetItemIdx(action.itemData.menuId, state);
+      const newState = [...state];
+      newState[targetItemIdx] = action.itemData;
+      return newState;
+    }
+    case 'ADD':
+      return [...state, action.itemData];
+    default:
+      throw new Error('Invalid action');
+  }
+}
