4f8da66e39e95337252feef387bedc39f34eabd6
happyGyu <happyboyto.dev@gmail.com>
2022-08-10 02:31:08 +0900

    feat: 랭킹 마커 구현
    - 매출이 없으면 순위 표시하지 않음
    - 동순위는 별도로 처리하지 않음
    
    - #22

---
+++ client/src/components/MenuList/MenuItem/RankingMarker/index.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { colors } from 'style/constants';
+import mixin from 'style/mixin';
+import policy from 'policy';
+
+interface IRankingMarker {
+  ranking: number;
+}
+
+const markColors = {
+  PRIMARY: { color: colors.primary, bgColor: colors.gold },
+  SECONDARY: { color: colors.secondary, bgColor: colors.silver },
+  TERTIARY: { color: colors.tertiary, bgColor: colors.bronze },
+  OTHER: { color: colors.offWhite, bgColor: colors.darkGrey },
+};
+
+export default function RankingMarker({ ranking }: IRankingMarker) {
+  const decideMarkColors = (target: number) => {
+    if (target <= policy.PRIMARY_RANK_RANGE) {
+      return markColors.PRIMARY;
+    }
+    if (target <= policy.SECONDARY_RANK_RANGE) {
+      return markColors.SECONDARY;
+    }
+    if (target <= policy.TERTIARY_RANK_RANKGE) {
+      return markColors.TERTIARY;
+    }
+    return markColors.OTHER;
+  };
+  const { color, bgColor } = decideMarkColors(ranking);
+  return (
+    <RankingMark ranking={ranking} color={color} bgColor={bgColor}>
+      {ranking}위
+    </RankingMark>
+  );
+}
+
+const RankingMark = styled.div<{
+  ranking: number;
+  bgColor: string;
+  color: string;
+}>`
+  opacity: ${({ ranking }) => (ranking ? 0.8 : 0)};
+  transition: opacity 1s;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 25%;
+  font-size: 1.25rem;
+  font-weight: 600;
+  ${mixin.flexMixin({ justify: 'center', align: 'center' })}
+  background-color: ${({ bgColor }) => bgColor};
+  color: ${({ color }) => color};
+`;
