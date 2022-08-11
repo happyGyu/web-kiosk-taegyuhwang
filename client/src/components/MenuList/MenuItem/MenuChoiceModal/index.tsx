1885d294794b0641c5afba3e52786d40eb4616fd
happyGyu <happyboyto.dev@gmail.com>
2022-08-11 22:54:19 +0900

    feat: 장바구니로 날아가는 애니메이션 구현
    - #43

--- client/src/components/MenuList/MenuItem/MenuChoiceModal/index.tsx
+++ client/src/components/MenuList/MenuItem/MenuChoiceModal/index.tsx
@@ -6,7 +6,7 @@ import mixin from 'style/mixin';
 import { ChoiceIdType, GetChoicesApiResponseDto, IChoice, IMenu } from 'types';
 import MenuThumbnail from 'components/common/MenuThumbnail';
 import useAxios from 'hooks/useAxios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import QuantityController from 'components/QuantityController';
 import {
   useCartDispatchContext,
@@ -16,6 +16,7 @@ import policy from 'policy';
 import CommonModalHeader from 'components/Modal/CommonModalHeader';
 import CommonModalButtons from 'components/Modal/CommonModalButtons';
 import { formatMoneyString } from 'utils';
+import portalStore from 'store/portal';
 import ChoiceGroup from './ChoiceGroup';
 
 interface IMenuChoiceModal extends IMenu {
@@ -30,6 +31,12 @@ interface IUserChoices {
   [key: ChoiceIdType]: IUserChoice;
 }
 
+type TFlightInfo = {
+  isFlying: boolean;
+  startTop: number;
+  startLeft: number;
+};
+
 export default function MenuChoiceModal({
   id,
   name,
@@ -40,11 +47,16 @@ export default function MenuChoiceModal({
   const { data: choiceGroups, isLoading } = useAxios<GetChoicesApiResponseDto>(
     `/choices?menu-id=${id}`
   );
-
+  const [topGunFlightInfo, setTopGunFlightInfo] = useState<TFlightInfo>({
+    isFlying: false,
+    startTop: 0,
+    startLeft: 0,
+  });
   const [userChoices, setUserChoices] = useState<IUserChoices | null>(null);
   const [quantity, setQuantity] = useState<number>(1);
   const cartState = useCartStateContext();
   const dispatchCart = useCartDispatchContext();
+  const menuNameRef = useRef<HTMLDivElement | null>(null);
 
   const isSameChoices = (aChoices: IChoice[], bChoices: IChoice[]) => {
     if (aChoices.length !== bChoices.length) return false;
@@ -87,8 +99,11 @@ export default function MenuChoiceModal({
   };
 
   const handleAddButtonClick = () => {
-    addToCart();
-    closeModal();
+    setTopGunFlightInfo((prev) => ({ ...prev, isFlying: true }));
+    setTimeout(() => {
+      addToCart();
+      closeModal();
+    }, 1000);
   };
 
   const selectChoice = (groupId: number, choice: IChoice) => {
@@ -121,6 +136,13 @@ export default function MenuChoiceModal({
   };
 
   useEffect(() => {
+    if (!menuNameRef.current) return;
+    const { top, left } = menuNameRef.current.getBoundingClientRect();
+    const newInfo = { ...topGunFlightInfo, startTop: top, startLeft: left };
+    setTopGunFlightInfo(newInfo);
+  }, [menuNameRef]);
+
+  useEffect(() => {
     if (isLoading || !choiceGroups) return;
     const initialUserChoices = choiceGroups.reduce(
       (obj, choiceGroup) => ({
@@ -135,7 +157,12 @@ export default function MenuChoiceModal({
     setUserChoices(initialUserChoices);
   }, [isLoading]);
 
-  return (
+  const Portal = portalStore.makePortal();
+  return topGunFlightInfo.isFlying ? (
+    <Portal>
+      <TopGunThumbnail src={imgUrl} topGunFlightInfo={topGunFlightInfo} />
+    </Portal>
+  ) : (
     <CustomModal closeModal={closeModal}>
       <CommonModalHeader>
         <h2>옵션 선택</h2>
@@ -146,7 +173,7 @@ export default function MenuChoiceModal({
           width="40%"
         >
           <MenuThumbnail size="L" imgUrl={imgUrl} />
-          <MenuName>{name}</MenuName>
+          <MenuName ref={menuNameRef}>{name}</MenuName>
           <TotalPrice>
             {formatMoneyString(caculateMenuPrice() * quantity)}
           </TotalPrice>
@@ -206,4 +233,30 @@ const TotalPrice = styled.span`
   font-size: 2.25rem;
   font-weight: 700;
   margin: 3rem 0 1.5rem 0;
+`;
+
+const TopGunThumbnail = styled.img<{ topGunFlightInfo: TFlightInfo }>`
+  position: fixed;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  animation: flight 1s forwards;
+
+  @keyframes flight {
+    from {
+      top: ${({ topGunFlightInfo }) => `${topGunFlightInfo.startTop}px`};
+      left: ${({ topGunFlightInfo }) => `${topGunFlightInfo.startLeft}px`};
+      width: 10rem;
+      height: 10rem;
+      opacity: 1;
+    }
+    to {
+      top: ${({ topGunFlightInfo }) => `${topGunFlightInfo.startTop - 100}px`};
+      left: ${({ topGunFlightInfo }) =>
+        `${topGunFlightInfo.startLeft + 400}px`};
+      width: 3rem;
+      height: 3rem;
+      opacity: 0.3;
+    }
+  }
 `;
